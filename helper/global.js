c1eeb0e826cf95f56a7a4ee2918351aea721fd1f
guy <guy.t.zero@gmail.com>
2022-03-29 20:43:13 +0700

    add addTwoNumbers fun

--- helper/global.js
+++ helper/global.js
@@ -25,4 +25,58 @@ export default {
     let final = Array.from(res);
     return final.length > 2 ? [final[0], final[1]] : final;
   },
+
+  addTwoNumbers(l1, l2) {
+    /**
+     * Definition for singly-linked list.
+     * function ListNode(val, next) {
+     *     this.val = (val===undefined ? 0 : val)
+     *     this.next = (next===undefined ? null : next)
+     * }
+     */
+    // /**
+    //  * @param {ListNode} l1
+    //  * @param {ListNode} l2
+    //  * @return {ListNode}
+    //  */
+    // Initialise a new ListNode to be returned
+    var newListNode = new ListNode(0);
+    var headOfNewListNode = newListNode;
+
+    // Initialise variables to be utilised on each run
+    var sum = 0;
+    var carry = 0;
+
+    // While there are elements (or a carried number) to add
+    while (l1 !== null || l2 !== null || sum > 0) {
+      // If there's an element in l1 to be added, add it to the sum and move to the next l1 node
+      if (l1 !== null) {
+        sum = sum + l1.val;
+        l1 = l1.next;
+      }
+
+      // If there's an element in l2 to be added, add it to the sum and move to the next l2 node
+      if (l2 !== null) {
+        sum = sum + l2.val;
+        l2 = l2.next;
+      }
+
+      // Check if the sum for these nodes exceeds 10
+      if (sum >= 10) {
+        carry = 1;
+        sum = sum - 10;
+      }
+
+      // Add the sum to the new ListNode, and then move it to the next entry
+      headOfNewListNode.next = new ListNode(sum);
+      headOfNewListNode = headOfNewListNode.next;
+
+      // Set the sum for the next addition to equal the carry-over (if there was one)
+      sum = carry;
+      carry = 0;
+    }
+
+    // Return the constructed ListNode (ignoring the first dummy entry)
+    return newListNode.next;
+  },
 };
