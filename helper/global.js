6ee057c295323442e1aea15b33f17b5f6456de56
guy <guy.t.zero@gmail.com>
2022-04-04 19:52:17 +0700

    add strStr fun

--- helper/global.js
+++ helper/global.js
@@ -437,4 +437,17 @@ export default {
 
     return i + 1;
   },
+  strStr(haystack, needle) {
+    if (needle.length === 0) return 0;
+
+    if (needle === haystack) return 0;
+
+    for (let i = 0; i <= haystack.length - needle.length; i++) {
+      if (needle === haystack.substring(i, i + needle.length)) {
+        return i;
+      }
+    }
+
+    return -1;
+  },
 };
