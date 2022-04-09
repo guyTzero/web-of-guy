d7f7a812bccddac2e72fb5abdf5f3c91541f2ca8
guy <guy.t.zero@gmail.com>
2022-04-09 13:31:36 +0700

    add addBinary fun

--- helper/global.js
+++ helper/global.js
@@ -574,4 +574,37 @@ export default {
     let answer = number.reverse();
     return answer;
   },
+  addBinary(a, b) {
+    //   /**
+    //  * @param {string} a
+    //  * @param {string} b
+    //  * @return {string}
+    //  */
+    let result = "";
+    let i = a.length - 1;
+    let j = b.length - 1;
+    let carry = 0;
+    while (i >= 0 || j >= 0) {
+      console.log("----" + "i = " + i + " | j = " + j + "------");
+      let sum = carry;
+      console.log("sum => ", sum);
+      if (i >= 0) {
+        sum += a[i--] - "0";
+        console.log("sum in i => ", sum);
+      }
+      if (j >= 0) {
+        sum += b[j--] - "0";
+        console.log("sum in j => ", sum);
+      }
+      result = (sum % 2) + result;
+      console.log("result => ", result);
+      carry = parseInt(sum / 2);
+      console.log("carry => ", carry);
+    }
+    if (carry > 0) {
+      result = 1 + result;
+    }
+    console.log("final => ", result);
+    return result;
+  },
 };
