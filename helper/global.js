858b1693e2169c3d5fa1cc46e7f83218beb68b09
guy <guy.t.zero@gmail.com>
2022-04-11 16:29:41 +0700

    add mySqrt fun

--- helper/global.js
+++ helper/global.js
@@ -607,4 +607,17 @@ export default {
     console.log("final => ", result);
     return result;
   },
+  mySqrt(x) {
+    let left = 1;
+    let right = x;
+    if (x < 2) return x;
+
+    while (left < right) {
+      const mid = Math.floor((left + right) / 2);
+      if (mid * mid === x) return mid;
+      else if (mid * mid > x) right = mid;
+      else left = mid + 1;
+    }
+    return left - 1;
+  },
 };
