b6c2644e6774fe57f45b49a2c9a4248723d2f2ed
guy <guy.t.zero@gmail.com>
2022-04-14 22:23:28 +0700

    add deleteDuplicates fun

--- helper/global.js
+++ helper/global.js
@@ -637,4 +637,28 @@ export default {
     }
     return second;
   },
+  /**
+   * Definition for singly-linked list.
+   * function ListNode(val, next) {
+   *     this.val = (val===undefined ? 0 : val)
+   *     this.next = (next===undefined ? null : next)
+   * }
+   */
+  // /**
+  //  * @param {ListNode} head
+  //  * @return {ListNode}
+  //  */
+  deleteDuplicates(head) {
+    if (!head) return head;
+    var cur = head;
+    while (cur.next != null) {
+      var nextnode = cur.next;
+      if (cur.val == nextnode.val) {
+        cur.next = cur.next.next;
+      } else {
+        cur = cur.next;
+      }
+    }
+    return head;
+  },
 };
