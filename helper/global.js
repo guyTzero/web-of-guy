d7c92687348ddc32696a48bc7063308fc044d403
guy <guy.t.zero@gmail.com>
2022-04-15 12:34:16 +0700

    add inorderTraversal fun

--- helper/global.js
+++ helper/global.js
@@ -661,4 +661,33 @@ export default {
     }
     return head;
   },
+  //   /**
+  //  * Definition for a binary tree node.
+  //  * function TreeNode(val, left, right) {
+  //  *     this.val = (val===undefined ? 0 : val)
+  //  *     this.left = (left===undefined ? null : left)
+  //  *     this.right = (right===undefined ? null : right)
+  //  * }
+  //  */
+  // /**
+  //  * @param {TreeNode} root
+  //  * @return {number[]}
+  //  */
+  inorderTraversal(root) {
+    var stack = [];
+    var now = root;
+    var res = [];
+
+    while (now || stack.length) {
+      while (now) {
+        stack.push(now);
+        now = now.left;
+      }
+      now = stack.pop();
+      res.push(now.val);
+      now = now.right;
+    }
+
+    return res;
+  },
 };
