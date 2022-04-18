72700ec69a700b1700460e5e0af70b518e0aded6
guy <guy.t.zero@gmail.com>
2022-04-18 22:14:55 +0700

    add isSymmetric fun

--- helper/global.js
+++ helper/global.js
@@ -811,4 +811,25 @@ export default {
     }
     return box[0];
   },
+  isSymmetric(root) {
+    //   /**
+    //  * Definition for a binary tree node.
+    //  * function TreeNode(val, left, right) {
+    //  *     this.val = (val===undefined ? 0 : val)
+    //  *     this.left = (left===undefined ? null : left)
+    //  *     this.right = (right===undefined ? null : right)
+    //  * }
+    //  */
+    // /**
+    //  * @param {TreeNode} root
+    //  * @return {boolean}
+    //  */
+    function helper(p, q) {
+      if ((!p && q) || (p && !q) || (p && q && p.val !== q.val)) return false;
+      if (p && q) return helper(p.left, q.right) && helper(p.right, q.left);
+      return true;
+    }
+    if (!root) return true;
+    return helper(root.left, root.right);
+  },
 };
