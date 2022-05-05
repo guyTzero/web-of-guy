ab19573850745a7504a570a13f601c82af9e5f78
guy <guy.t.zero@gmail.com>
2022-05-05 07:38:07 +0700

    add implement-stack-using-queues fun

--- helper/global.js
+++ helper/global.js
@@ -1438,4 +1438,66 @@ export default {
     }
     return dup;
   },
+  implement_stack_using_queues() {
+    var Queue = function () {
+      const queue = [];
+      return {
+        get length() {
+          return queue.length;
+        },
+        push: (x) => queue.push(x),
+        pop: () => queue.shift(),
+        top: () => queue[0],
+      };
+    };
+
+    var MyStack = function () {
+      this.queue = Queue();
+    };
+
+    /**
+     * @param {number} x
+     * @return {void}
+     */
+    MyStack.prototype.push = function (x) {
+      if (this.queue.length === 0) {
+        this.queue.push(x);
+      } else {
+        this.queue.push(x);
+        for (let i = 1; i <= this.queue.length - 1; i++) {
+          this.queue.push(this.queue.pop());
+        }
+      }
+    };
+
+    /**
+     * @return {number}
+     */
+    MyStack.prototype.pop = function () {
+      return this.queue.pop();
+    };
+
+    /**
+     * @return {number}
+     */
+    MyStack.prototype.top = function () {
+      return this.queue.top();
+    };
+
+    /**
+     * @return {boolean}
+     */
+    MyStack.prototype.empty = function () {
+      return this.queue.length === 0;
+    };
+
+    /**
+     * Your MyStack object will be instantiated and called as such:
+     * var obj = new MyStack()
+     * obj.push(x)
+     * var param_2 = obj.pop()
+     * var param_3 = obj.top()
+     * var param_4 = obj.empty()
+     */
+  },
 };
