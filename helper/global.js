470e119bb997034cb85a94aa6036e00a75923cdf
guyTzero <98171911+guyTzero@users.noreply.github.com>
2022-05-09 18:39:28 +0700

    Add containsNearbyDuplicate fun

--- helper/global.js
+++ helper/global.js
@@ -1605,5 +1605,21 @@ export default {
     NestedIterator.prototype.next = function () {
       return this.list[this.count++];
     };
-  },
+  }, containsNearbyDuplicate (nums, k) {
+  const map = new Map();
+
+  for (let i = 0; i < nums.length; i++) {
+    if (map.has(nums[i])) {
+      const j = map.get(nums[i]);
+      
+      if (Math.abs(i - j) <= k) {
+        return true;
+      }
+    }
+
+    map.set(nums[i], i);
+  }
+
+  return false;
+}
 };
