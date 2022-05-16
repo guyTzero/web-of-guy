a6740465a09ac3e93f29ddcefa1389c108a61da2
guy <guy.t.zero@gmail.com>
2022-05-16 17:56:12 +0700

    add shortestPathBinaryMatrix fun

--- helper/global.js
+++ helper/global.js
@@ -1863,4 +1863,40 @@ export default {
     // console.log(ret)
     return ret;
   },
+  maxProfit(prices) {
+    // my very slow code
+    let ret = [];
+    for (let i = 0; i < prices.length; i++) {
+      for (let k = i + 1; k < prices.length; k++) {
+        if (prices[i] < prices[k]) {
+          ret.push(prices[k] - prices[i]);
+        }
+      }
+    }
+
+    // console.log("ret", ret.length);
+
+    return ret.length > 0 ? Math.max(...ret) : 0;
+  },
+  shortestPathBinaryMatrix(grid) {
+    /**
+     * @param {number[][]} grid
+     * @return {number}
+     */
+    let n = grid.length - 1,
+      q = [0];
+    if (grid[0][0] || grid[n][n]) return -1;
+    grid[0][0] = 1;
+    while (q.length) {
+      let curr = q.shift(),
+        i = curr & ((1 << 7) - 1),
+        j = curr >> 7;
+      if (i === n && j === n) return grid[n][n];
+      for (let a = Math.max(i - 1, 0); a <= Math.min(i + 1, n); a++)
+        for (let b = Math.max(j - 1, 0); b <= Math.min(j + 1, n); b++)
+          if (grid[a][b] === 0)
+            (grid[a][b] = grid[i][j] + 1), q.push(a + (b << 7));
+    }
+    return -1;
+  },
 };
