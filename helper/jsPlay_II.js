11511d7aa7f84cca245755cbeeefce1e1c620098
guy <guy.t.zero@gmail.com>
2022-06-10 18:09:53 +0700

    add lengthOfLongestSubstring fun

--- helper/jsPlay_II.js
+++ helper/jsPlay_II.js
@@ -289,4 +289,46 @@ export default {
     }
     return res;
   },
+  lengthOfLongestSubstringSlow(s) {
+    if (!s) {
+      return 0;
+    }
+    let c = 1;
+    for (let i = 1; i < s.length; i++) {
+      for (let j = 0; j < i; j++) {
+        let str = s.split("");
+        str = str.slice(j, i + 1);
+        let _set = new Set(str);
+        _set = Array.from(_set);
+        if (str.length == _set.length) {
+          c = c > str.length ? c : str.length;
+        }
+      }
+    }
+    return c;
+  },
+  lengthOfLongestSubstringFast(s) {
+    // Store counters for the biggest string, the start of the window, and the current letter's position (end of window)
+    let longestStringLength = 0,
+      startOfWindow = 0,
+      currentPosition = 0;
+
+    // Initialise a Set to store the characters
+    let characterSet = new Set();
+
+    // Loop through the provided string
+    while (currentPosition < s.length) {
+      // Check if the current character exists in the Set
+      if (characterSet.has(s[currentPosition])) {
+        // If so, delete it from the Set (as it will be picked up on the next run), and move the window's start forwards
+        characterSet.delete(s[startOfWindow++]);
+      } else {
+        // If not, add the current character to the Set, and move the current character forwards
+        characterSet.add(s[currentPosition++]);
+        longestStringLength = Math.max(longestStringLength, characterSet.size);
+      }
+    }
+
+    return longestStringLength;
+  },
 };
