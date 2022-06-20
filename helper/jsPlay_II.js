9334e22a6ddaf802724927536dc64446a826aa81
guy <guy.t.zero@gmail.com>
2022-06-20 23:58:20 +0700

    add sumOddLengthSubarrays fun

--- helper/jsPlay_II.js
+++ helper/jsPlay_II.js
@@ -408,4 +408,21 @@ export default {
     }
     return ans;
   },
+  sumOddLengthSubarrays(arr) {
+    // Stores the sum
+    let sum = 0;
+
+    // Size of array
+    let l = arr.length;
+
+    // Traverse the given array arr[]
+    for (let i = 0; i < l; i++) {
+      // Add to the sum for each
+      // contribution of the arr[i]
+      sum += Math.floor(((i + 1) * (l - i) + 1) / 2) * arr[i];
+    }
+
+    // Return the final sum
+    return sum;
+  },
 };
