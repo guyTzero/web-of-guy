cd98a8078893a2f27a2a05b5d0205f29d9fa4c4c
guyTzero <98171911+guyTzero@users.noreply.github.com>
2022-08-30 23:58:29 +0700

    add minDeletionSize fun

--- helper/jsPlay_II.js
+++ helper/jsPlay_II.js
@@ -1073,5 +1073,34 @@ export default {
         s = s.substring(1);
     }
     return count
+},
+  minDeletionSize(strs) {
+    function arraysEqual(a, b) {
+      if (a === b) return true;
+      if (a == null || b == null) return false;
+      if (a.length !== b.length) return false;
+
+      for (var i = 0; i < a.length; ++i) {
+        if (a[i] !== b[i]) return false;
+      }
+      return true;
+    }
+    let arr = []
+    for(let i = 0 ; i < strs[0].length ; i++){
+        let prep = strs.map(e => e[i])
+        arr.push(prep)
+    }
+
+    return   arr.map(e => {
+        let a = e.map(e => e)
+        let b = e.sort()
+        if(arraysEqual(a,b)){
+           
+            return true
+           }else{
+               false
+           }
+    }).filter(e => !e).length
+    
 }
 };
