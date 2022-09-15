37429cc14b993a2b41d5fcac4845ed9f49b05632
guyTzero <98171911+guyTzero@users.noreply.github.com>
2022-09-15 19:29:58 +0700

    add countPairs fun

--- helper/jsPlay_II.js
+++ helper/jsPlay_II.js
@@ -1302,5 +1302,16 @@ export default {
     }
   }
   return sol;
+},
+  countPairs(nums,k ) {
+    let result = 0;
+    for (let i = 0; i < nums.length; i++) {
+        for (let j = i + 1; j < nums.length; j++) {
+            if (nums[i] === nums[j] && (i * j) % k === 0) {
+                result++;
+            }
+        }
+    }
+    return result;
 }
 };
