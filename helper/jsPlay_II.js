c47dd405457a32f7f644e9c1dc4ddd5aecf0719f
guyTzero <98171911+guyTzero@users.noreply.github.com>
2022-09-20 22:05:44 +0700

    add minOperations fun

--- helper/jsPlay_II.js
+++ helper/jsPlay_II.js
@@ -1314,5 +1314,20 @@ export default {
   },
   flipAndInvertImage(image) {
     return image.map(e => e.reverse().map(ele =>  ele ? 0 : 1))
+  },
+  minOperations(nums) {
+        let ans = 0;
+        for (let i = 1; i < nums.length; i++) {
+            let previous = nums[i-1];
+            let current = nums[i];
+            
+            if (current <= previous) {
+                current = previous + 1;
+                ans += (current - nums[i]);
+                nums[i] = current;
+            }
+        }
+        
+        return ans
   }
 };
