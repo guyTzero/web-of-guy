811f699e99c54d3f53e746f6b8b75a3845c02606
guyTzero <98171911+guyTzero@users.noreply.github.com>
2022-10-04 16:42:01 +0700

    add sortedArrayToBST func

--- helper/jsPlay_II.js
+++ helper/jsPlay_II.js
@@ -1426,5 +1426,17 @@ export default {
         }
     }
     return true;
+  },
+  sortedArrayToBST(nums) {
+    if (!nums.length) return null;
+
+    const mid = Math.floor(nums.length / 2);
+    const root = new TreeNode(nums[mid]);
+
+    // Call the function recursively on each subtree
+    root.left = sortedArrayToBST(nums.slice(0, mid));
+    root.right = sortedArrayToBST(nums.slice(mid + 1));
+
+    return root
   }
 };
