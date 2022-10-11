c5a1901c5d76c186369b70c94914258fc75313a7
guyTzero <98171911+guyTzero@users.noreply.github.com>
2022-10-11 21:41:23 +0700

    add projectionArea func

--- helper/jsPlay_II.js
+++ helper/jsPlay_II.js
@@ -1498,5 +1498,27 @@ export default {
         count = 1;
     }
     return result
+  },
+  projectionArea(grid) {
+    
+    const xy = grid.reduce((acc,curr)=>{
+        const area = curr.filter(value => value>0).length
+        return acc + area
+    },0)
+    
+    const yz = grid.reduce((acc,curr) => {
+        const max =  Math.max(...curr) 
+        return acc + max
+      },0)
+
+    const xz = grid[0].reduce((acc,curr,index) => {
+        const vertical =  grid.map(value =>{
+          return value[index]
+        })
+        const max =  Math.max(...vertical) 
+        return acc + max
+      },0)
+
+      return xy+yz+xz 
   }
 };
