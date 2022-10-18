0b10c06df1de5c42b0382b9a1a9c91f2199f5efe
guyTzero <98171911+guyTzero@users.noreply.github.com>
2022-10-18 07:41:31 +0700

    add generatePascalsTriangle func

--- helper/jsPlay_II.js
+++ helper/jsPlay_II.js
@@ -1555,5 +1555,19 @@ export default {
         }
         }
         return true;
+    },
+    generatePascalsTriangle(numRows) {
+       let ret = [];
+    
+    for(let i = 0; i < numRows; i++)
+    {
+        ret[i] = [];
+        ret[i][0] = ret[i][i] = 1;
+        for(let j = 1; j < i; j++)
+        {
+               ret[i][j] = ret[i-1][j] + ret[i-1][j-1]; 
+        }
     }
+    return ret; 
+      }
 };
