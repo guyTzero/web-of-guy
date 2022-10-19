e960cc58e064b0d2bf889d29a405a022fba9fc60
guyTzero <98171911+guyTzero@users.noreply.github.com>
2022-10-19 07:39:10 +0700

    add subsetXORSum func

--- helper/jsPlay_II.js
+++ helper/jsPlay_II.js
@@ -1569,5 +1569,12 @@ export default {
         }
     }
     return ret; 
-      }
+      },
+         subsetXORSum (nums) {
+   let bitOR = 0;
+   for (let  i=0; i < nums.length; ++i) {
+       bitOR |= nums[i]; 
+   }
+   return (bitOR * Math.pow(2, nums.length-1));
+}
 };
