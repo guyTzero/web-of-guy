d477bfeb2302b712fa3091e68ae4f69ac8660b8c
guyTzero <98171911+guyTzero@users.noreply.github.com>
2022-10-20 17:35:30 +0700

    add countPoints func

--- helper/jsPlay_II.js
+++ helper/jsPlay_II.js
@@ -1576,5 +1576,23 @@ export default {
        bitOR |= nums[i]; 
    }
    return (bitOR * Math.pow(2, nums.length-1));
+},
+ countPoints  (rings) {
+    let n = rings.length;
+
+    let map = new Map(); // map of  rod nums  to their  set of colors
+    for (let i = 0; i < n; i += 2) {
+        let color = rings[i];
+        let rod = +rings[i + 1];
+
+        if (!map.has(rod)) map.set(rod, new Set());
+        map.get(rod).add(color);
+    }
+
+    let count = 0;
+    for (let [rod, setColors] of map) {
+        if (setColors.size == 3) count++;
+    }
+    return count;
 }
 };
