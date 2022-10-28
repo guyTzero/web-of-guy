1f4608ff3960531d5abb4d2be4c592e6698676fc
guyTzero <98171911+guyTzero@users.noreply.github.com>
2022-10-28 23:39:54 +0700

    add twoSum func

--- helper/jsPlay_II.js
+++ helper/jsPlay_II.js
@@ -1632,5 +1632,14 @@ export default {
         }
     }
     return howTallCanWeGo
+},
+  twoSum(nums, target) {
+    for(let i = 0 ; i < nums.length ; i++){
+        for(let j = 0 ; j < nums.length ; j++){
+            if(i !== j && nums[i] + nums[j] == target){
+                return [i,j]
+            }
+        }
+    }
 }
 };
