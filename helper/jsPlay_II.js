f605c3aec52fd4cdfb68d5df411e66629ce4726d
guyTzero <98171911+guyTzero@users.noreply.github.com>
2022-11-02 17:20:30 +0700

    add garbageCollection func

--- helper/jsPlay_II.js
+++ helper/jsPlay_II.js
@@ -1682,5 +1682,33 @@ export default {
         }
     }
     return totalLength % 2 ? lastNum : (lastNum + secondToLastNum) / 2;
-}
+},
+  garbageCollection(garbage, travel) {
+    const exprense = [0, ];
+    for (let i = 0; i < travel.length; i++) {
+        exprense[i + 1] = exprense[i] + travel[i];
+    }
+    let M = 0;
+    let P = 0;
+    let G = 0;
+    let mi = 0;
+    let pi = 0;
+    let gi = 0;
+    for (let i = 0; i < garbage.length; i++) {
+        for (const c of garbage[i]) {
+            if (c === 'M') {
+                M++;
+                mi = i;
+            } else if (c === 'P') {
+                P++;
+                pi = i;
+            } else {
+                G++;
+                gi = i;
+            }
+        }
+    }
+
+    return M + P + G + exprense[mi] + exprense[pi] + exprense[gi];
+  }
 };
