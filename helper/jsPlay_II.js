626c63d575c3a93b13d78e793c3bdc436a357ef9
guyTzero <98171911+guyTzero@users.noreply.github.com>
2022-11-06 20:27:13 +0700

    add earliestFullBloom func

--- helper/jsPlay_II.js
+++ helper/jsPlay_II.js
@@ -1748,5 +1748,23 @@ let dfs = (sx,sy,cnt,grid) => {
     grid[sx][sy] = 0  
     return res
 }
+},
+   earliestFullBloom (plantTime, growTime) {
+  let n = plantTime.length;
+  let plants = [];
+  for (var i = 0; i < n; i++) {
+    plants.push([plantTime[i], growTime[i]]);
+  }
+  plants.sort((a, b) => {
+    if (a[1] === b[1]) return b[0] - a[0];
+    return b[1] - a[1];
+  });
+
+  let time = 0, fullBloomTime = 0;
+  for (i = 0; i < n; i++) {
+    time += plants[i][0];
+    fullBloomTime = Math.max(fullBloomTime, time + plants[i][1]);
+  }
+  return fullBloomTime;
 }
 };
