d7e36fcc9c6fd8a710958a269b0c0075e9c9bed3
guyTzero <98171911+guyTzero@users.noreply.github.com>
2022-12-04 12:52:06 +0700

    add getRow func

--- helper/jsPlay_II.js
+++ helper/jsPlay_II.js
@@ -1965,5 +1965,13 @@ var getMaxIndex = function(arr){
     }
 
     return result;
+},
+  getRow(rowIndex) {
+      var res = Array(rowIndex + 1);
+  res[0] = 1;
+  for (var i = 1; i <= rowIndex; i++) {
+    res[i] = res[i - 1] * ((rowIndex - i + 1) / i);
+  }
+  return res;
 }
 };
