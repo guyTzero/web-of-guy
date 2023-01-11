c85ae698147403b7dd60b236793a3ce33fb06f78
guyTzero <98171911+guyTzero@users.noreply.github.com>
2023-01-11 11:27:51 +0700

    add threeSumClosest func

--- helper/jsPlay_II.js
+++ helper/jsPlay_II.js
@@ -2020,5 +2020,42 @@ var getMaxIndex = function(arr){
     maxDepth(root) {
     if (root === null) return 0;
     return 1 + Math.max(maxDepth(root.left) , maxDepth(root.right));
+    },
+   threeSumClosest = (nums, target) => {
+  let len = nums.length, sum=0;
+  if(len === 0) return 0;
+  if(len <= 3) {
+    for(let i=0; i<len; i++){
+        sum += nums[i];
     }
+    return sum;
+  }
+  nums.sort(function(a,b){
+      return a-b;
+  });
+  
+  let closest = nums[0]+nums[1]+nums[2];
+  for(let i=0; i<len; i++){
+    for(let j=i+1, k=len-1; j<len-1, j<k; ){
+      sum = nums[i] + nums[j] + nums[k];
+      if(sum === target){
+          return sum;
+      } else if(sum<target){
+        if((closest<sum && sum<target) ||
+           Math.abs(target-sum)<Math.abs(target-closest)){
+            closest = sum;
+        }
+        j++;
+      } else if(sum>target){
+        if(closest>sum && sum>target ||
+           Math.abs(target-sum) < Math.abs(target-closest)) {
+            closest = sum;
+        }
+        k--; //to reduce sum decrease tail
+      }
+        
+    }
+  }
+  return closest;
+  }
 };
