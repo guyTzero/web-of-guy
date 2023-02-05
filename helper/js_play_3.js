8f271a3a368562a5e02a6d2e8d9f21fd06035730
guyTzero <98171911+guyTzero@users.noreply.github.com>
2023-02-05 20:46:30 +0700

    add removeNthFromEnd func

--- helper/js_play_3.js
+++ helper/js_play_3.js
@@ -154,6 +154,26 @@ letterCombinations(digits) {
     }
 
     return circularSandwiches + squareSandwiches;
+},
+ removeNthFromEnd(head, n) {
+    let newLinkedList = new ListNode(0);
+    newLinkedList.next = head;
+
+    let fastPointer = newLinkedList;
+    let slowPointer = newLinkedList;
+
+    for (let i = 1; i <= n + 1; i++) {
+        fastPointer = fastPointer.next;
+    }
+
+    while (fastPointer != null) {
+        fastPointer = fastPointer.next;
+        slowPointer = slowPointer.next;
+    }
+
+    slowPointer.next = slowPointer.next.next;
+
+    return newLinkedList.next;
 }
 
 }
