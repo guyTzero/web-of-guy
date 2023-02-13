bf8f6ec6f48a81c7e14f0c93a32729e9cf2d6cb7
guyTzero <98171911+guyTzero@users.noreply.github.com>
2023-02-13 07:42:20 +0700

    add hasPathSum func

--- helper/js_play_3.js
+++ helper/js_play_3.js
@@ -318,5 +318,16 @@ validSudoku(board) {
     }
     
     return true;
+},
+ hasPathSum(root, targetSum) {
+    if (!root) {
+        return false;
+    }
+    
+    if (!root.left && !root.right && targetSum - root.val === 0) {
+        return true;
+    }
+    
+    return hasPathSum(root.left, targetSum - root.val) || hasPathSum(root.right, targetSum - root.val);
 }
 }
