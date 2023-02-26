785e1e21908e2e7a42446b722d2b381764f6ced2
guyTzero <98171911+guyTzero@users.noreply.github.com>
2023-02-26 14:01:50 +0700

    add combinationSum2 func

--- helper/js_play_3.js
+++ helper/js_play_3.js
@@ -384,5 +384,24 @@ export default {
     },
     someFunc(){
         return 'some val'
-    }
+    },
+combinationSum2(candidates, target) {
+  var dfs = function (res, stack, index, len, candidates, target) {
+  var tmp = null;
+  if (target < 0) return;
+  if (target === 0) return res.push(stack);
+  for (var i = index; i < len; i++) {
+    if (candidates[i] > target) break;
+    if (i > index && candidates[i] === candidates[i - 1]) continue;
+    tmp = Array.from(stack);
+    tmp.push(candidates[i]);
+    dfs(res, tmp, i + 1, len, candidates, target - candidates[i]);
+  }
+};
+  var res = [];
+  var len = candidates.length;
+  candidates.sort((a, b) => (a - b));
+  dfs(res, [], 0, len, candidates, target);
+  return res;
+}
 }
