25983ff8f298ff5cd1fa8960225d32da937cee67
guyTzero <98171911+guyTzero@users.noreply.github.com>
2023-03-14 17:53:34 +0700

    add permute func

--- helper/js_play_3.js
+++ helper/js_play_3.js
@@ -469,5 +469,23 @@ combinationSum2(candidates, target) {
         }
     }
     return jump;
+},
+    permute (choices, temp = [], permutations = []) {
+    // Base case 
+    if(choices.length === 0){
+      permutations.push([...temp]);
+    } 
+
+    for(let i = 0; i < choices.length; i++){
+        // Create new array without current letter
+        let newChoices = choices.filter((choice, index) => index !== i)
+        // Add current to the temp array which is our current permutation
+        temp.push(choices[i])
+        permute(newChoices, temp, permutations)
+        // Once we have explored options remove the current letter from our current permuataion
+        temp.pop()
+    }
+
+    return permutations
 }
 }
